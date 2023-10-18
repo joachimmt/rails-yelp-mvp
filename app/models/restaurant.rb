29bfdc719e9100d83540093f955a65154972537b
joachim miot <joachimmt55@gmail.com>
2023-10-17 20:55:59 -0400

    Restaurant, review

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,15 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true
+  validates :phone_number, presence: true
+
+  validate :exclude_inappropriate_words
+
+  def exclude_inappropriate_words
+    if category == "neptunian"
+      errors.add(:category, "contient une catégorie inappropriée")
+    end
+  end
 end
